30a3f17b0a143d9e1eb6f83954577c3377b9073e
daelbaz <daelbaz@g.jct.ac.il>
2022-10-27 18:39:06 +0300

    Initial code + Welcome functions

---
+++ dotNet5783_2065_7976/Stage0/Program2065.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2065();
+            Welcome7976();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome7976();
+        private static void Welcome2065()
+        {
+            Console.Write("Enter your name: ");
+            string user = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", user);
+        }
+    }
+}
