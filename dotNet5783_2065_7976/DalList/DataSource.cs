0ee8411f592fb8e62095fc3d433e1587b978a816
daelbaz <daelbaz@g.jct.ac.il>
2022-11-13 15:04:30 +0200

    Update

--- dotNet5783_2065_7976/DalList/DataSource.cs
+++ dotNet5783_2065_7976/DalList/DataSource.cs
@@ -1,9 +1,31 @@
-﻿namespace Dal;
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Dal;
 
 internal static class DataSource
 {
-  internal static readonly int Rand{ get; } = 2650;
-    OrderItem[] ori =      
-        
-   
+    //internal class Config
+    //{
+    //    internal static string ConfigPath { get; set; }   
+    //}
+    //internal static readonly int Rand{ get; } = 2650;
+    internal static int OI_capacity = 200;
+    internal static int P_capacity = 50;
+    internal static int O_capacity = 100;
+
+   internal static OrderItem[] OI_arr = new OrderItem[OI_capacity];
+   internal static Product[] P_arr = new Product[P_capacity];
+   internal static Order[] O_arr = new Order[O_capacity];
+
+    private static void OI_Initialize()
+    { for (int i = 0; i < OI_capacity; i++)
+        {
+           // OrderItem orderItem[] =;
+
+          
+
+
+         }
+    }
 }
