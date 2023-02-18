f6c92ba5bd39fce2967acaaa15540452f51142fd
uriehrlich <uriehrlich@gmail.com>
2023-02-18 22:44:44 +0200

    bonuses

---
+++ PL/Products/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.Products
+{
+    public static class Extensions
+    {
+        static BlApi.IBl? bl = BlApi.Factory.Get();
+        public static int InStock (this ProductForList p, int id)
+        {
+            return bl!.Product.GetProduct(id).InStock;
+        }
+    }
+}
